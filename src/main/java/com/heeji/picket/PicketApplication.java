2cc4a5224c626360784241b81ced8acd825ac21e
hyeom <h_uv8@naver.com>
2025-05-12 15:57:05 +0900

    Initial commit

---
+++ src/main/java/com/heeji/picket/PicketApplication.java
@@ -0,0 +1,13 @@
+package com.heeji.picket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heeji/picket/PicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeji.picket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
