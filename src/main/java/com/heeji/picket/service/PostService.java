7351a30afa7bdeb508552d542c24db559ecccf0c
hyeom <mchell9999@gmail.com>
2025-07-09 17:35:57 +0900

    불필요한 import 정리 (젠킨스 배포 테스트)

--- src/main/java/com/heeji/picket/service/PostService.java
+++ src/main/java/com/heeji/picket/service/PostService.java
@@ -6,12 +6,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
