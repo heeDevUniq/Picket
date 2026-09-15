7a70bba061e6dc6d1a5c8ee849a83b70efb0c3c1
hyeom <mchell9999@gmail.com>
2026-09-15 09:35:28 +0900

    구동 앱 세팅

--- src/main/java/com/heeji/picket/PicketApplication.java
+++ src/main/java/com/heeji/picket/PicketApplication.java
@@ -3,7 +3,9 @@ package com.heeji.picket;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.heeji.picket.mapper")
 public class PicketApplication {
