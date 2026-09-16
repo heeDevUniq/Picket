4f03b9f1df640ce33f632e67195f7bd6caafed8e
hyeom <mchell9999@gmail.com>
2026-09-16 13:11:29 +0900

    redis 연결, picket-queue(webflux) 기본 세팅

---
+++ picket-queue/src/main/java/com/heeji/picket_queue/PicketQueueApplication.java
@@ -0,0 +1,13 @@
+package com.heeji.picket_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicketQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicketQueueApplication.class, args);
+	}
+
+}
---
+++ picket-queue/src/test/java/com/heeji/picket_queue/PicketQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeji.picket_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicketQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
